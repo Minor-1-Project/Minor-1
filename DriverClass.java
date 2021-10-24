15c163abc32c98539aa3252e05bf11a74265c896
shrEYA12-12 <64414517+shrEYA12-12@users.noreply.github.com>
2021-10-24 13:38:05 +0530

    Add files via upload

---
+++ DriverClass.java
@@ -0,0 +1,122 @@
+package MinorPackage;
+import java.util.*;  
+import java.lang.*;
+public class DriverClass  {
+
+    static void display(int n, int m)
+	{
+		int i, j;
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j <= m; j++)
+			{
+                if (i==1 || i==n || j==1 || (j==m && i!=4 && i!=5))             
+                System.out.print("@");	
+                else if (i==4 && j==(m-32))
+                System.out.print("@");	   
+                else if (i==5 && j==(m-26))
+                System.out.print("@");                     
+                else if (i==4 && j==57)
+                System.out.print("Load Balancing in cloud computing");
+                else if (i==5 && j==57)
+                System.out.print("Mechanism of load balancing");
+                else
+                System.out.print(" "); 
+	
+			}
+			System.out.println();
+		}
+	
+	}
+    
+
+    public static void main(String args[]) {
+
+        int rows = 9, columns = 135; 
+        System.out.println("\t\t\t\t\t\t            MINOR 1 ");
+        System.out.println(); 
+       display(rows, columns); 
+        System.out.println(); 
+        System.out.println("\t\t\t\t\t\t\t           Approved by: ");
+        System.out.println();
+        System.out.println(" Mr. Amar Shukla  \t\t\t\t\t\t                                                Mr. Christalin Nelson S" );
+        System.out.println("       [MENTOR]  \t\t\t\t\t\t\t\t                                    [PANEL MEMBER]" );
+        System.out.println("\t\t\t\t\t\t\t           Submitted by: ");
+        System.out.println();
+        System.out.println("Ishika Mahawar [060]                     Sarthak Goyal [122] \t\t       Shreya Aggarwal [133]             \tVikas Mishra[149]" );
+        System.out.println();
+        
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the number of users:");
+        int n=sc.nextInt();
+        System.out.println("The number of users are: "+n);
+      
+        ArrayList<Integer> taskarr = new ArrayList<Integer>();
+        for(int i =0;i<n;i++)
+        {
+       	 System.out.println("Enter task number " +(i+1)+" to be performed:");
+        
+       	 taskarr.add(sc.nextInt());
+        }
+        System.out.println("ArrayList of users : " + taskarr);
+
+        ArrayList<Integer> prnarr = new ArrayList<Integer>();
+
+		
+        ArrayList<Integer> ranarr = new ArrayList<Integer>();
+
+		
+        ArrayList<Integer> dclb1arr = new ArrayList<Integer>();
+
+        List<Integer> parameter1=new ArrayList<Integer>();
+        List<Float> parameter2=new ArrayList<Float>();
+        List<Integer> sequenceList=new ArrayList<Integer>();
+ 
+        PriorityBasedRoundRobin obj=new PriorityBasedRoundRobin(n,taskarr);
+        ranarr =obj.randomFunc();
+       prnarr =obj.bubbleSort(ranarr,taskarr);
+       System.out.println("\n");
+       dclb1arr=(ArrayList<Integer>) obj.dcLoadbalancer1(prnarr);
+       int d =dclb1arr.size();
+      System.out.println("\n");
+      System.out.println("Enter the choice: \n1 for integer value input\n2 for float value input "); 
+     
+      System.out.println("Total number of users directed to Data Centre 1 LoadBalancer ----> "+d);
+      for(int i=0;i<d;i++)
+      {
+    	  System.out.println("Enter the task which the user "+dclb1arr.get(i)+"  has to perform");
+    	  System.out.println("Please enter your choice(1 or 2):");
+    	  int choice=sc.nextInt();
+    	  if (choice==1)
+    	  {
+    		  System.out.println("Enter an integer value");
+              parameter1.add(sc.nextInt());
+              sequenceList.add(choice);
+              
+    	  }
+    	  else if(choice==2)
+    	  {
+    		  System.out.println("Enter a float value");
+              parameter2.add(sc.nextFloat());
+              sequenceList.add(choice);
+
+    	  }
+    	  else {
+    		  System.out.println("wrong choice");
+    		  try {
+    	            Thread.sleep(2000);
+    	        } catch (InterruptedException e) {
+    	            
+    	            e.printStackTrace();
+    	        }
+    	        System.out.println("\033[H\033[2J");  
+    	        System.out.flush(); 
+    	  }
+      }
+
+      DC1LoadBalancer obj1=new DC1LoadBalancer(d,parameter1,parameter2,sequenceList );
+      obj1.least_workload1();
+
+    }
+ 
+}
